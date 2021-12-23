d4ef29608a223f92892961aeee6e45e64fce187d
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-12-23 20:22:04 -0300

    💻 fourth section

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import FirstSection from "../components/FirstSection";
 import SecondSection from "../components/SecondSection";
 import ThirdSection from "../components/ThirdSection";
+import FourthSection from "../components/FourthSection";
 
 function MainPage() {
   return (
@@ -8,6 +9,7 @@ function MainPage() {
       <FirstSection />
       <SecondSection />
       <ThirdSection />
+      <FourthSection />
     </>
     );
 }
---
+++ src/utils/testimonialsData.js
@@ -0,0 +1,26 @@
+import emilyPhoto from '../images/image-emily.jpg';
+import thomasPhoto from '../images/image-thomas.jpg';
+import jenniePhoto from '../images/image-jennie.jpg';
+
+const testimonials = [
+  {
+    name: 'Emily R.',
+    position: 'Marketing Director',
+    text: 'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.',
+    image: emilyPhoto
+  },
+  {
+    name: 'Thomas S.',
+    position: 'Chief Operating Officer',
+    text: 'Sunnyside\'s enthusiasm cupled with their keen interest in our brand\'s success made it a satisfying and enjoyable experience.',
+    image: thomasPhoto
+  },
+  {
+    name: 'Jennie F.',
+    position: 'Business Owner',
+    text: 'Incredible end result! OUr sales increased over 400% when we worked with Sunnyside. Highly recommended!',
+    image: jenniePhoto
+  }
+];
+
+export default testimonials;
