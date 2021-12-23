e9c4f5ef4527c09a2cae057e73485fc99ff7a376
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-12-23 16:34:30 -0300

    💻 third section

---
+++ src/components/ThirdSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function ThirdSection() {
+  return (
+    <div className="third-section">
+      <div className="graphic-design-container">
+        <div className="graphic-design-text-box">
+          <h2>Graphic Design</h2>
+          <p>
+            Great design makes you memorable. We deliver artwork that
+            underscores your brand message and captures potential clients
+            attention.
+          </p>
+        </div>
+      </div>
+      <div className="photography-container">
+        <div className="photography-text-box">
+          <h2>Photograpy</h2>
+          <p>
+            Increase your credibility by getting the most stunning, high-quality
+            photos that improve your business image.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ThirdSection;
